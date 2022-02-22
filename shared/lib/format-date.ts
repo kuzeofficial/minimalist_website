6df7f4f57c96609c99e4155bc657712eec69f371
Cristian Fonseca <cfonsecacomas@gmail.com>
2022-02-22 01:47:02 -0300

    Removed function for format date and purged dependencies

--- shared/lib/format-date.ts
+++
@@ -1,8 +0,0 @@
-import { format, parseISO } from "date-fns";
-
-type DateFormat =
-  | "yyyy-MM-dd"
-  | "yyyy-MM-dd HH:mm:ss"
-  | "yyyy-MM-dd HH:mm:ss.SSS";
-export const formatDate = (date: DateFormat) =>
-  format(parseISO(date), "MMMM d, yyyy");
